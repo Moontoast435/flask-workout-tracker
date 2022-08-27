a64e4227fbc7609c852c3c4d647f43b994f11372
Moontoast435 <seanmalone1993@gmail.com>
2022-08-27 17:19:50 +0100

    using sample data to test show route

--- workouts/routes/main.py
+++ workouts/routes/main.py
@@ -1,7 +1,16 @@
 from flask import Blueprint
-
+from flask import jsonify
 main_routes = Blueprint("example", __name__)
+
+all_workouts = [
+    {'id': 1, 'name': 'Squat', 'sets': [{'60kg': '2x8'}, {'70kg' : '3x10'}, {'65kg': '1x8'}]},
+    {'id': 2, 'name': 'Romanian DL', 'sets' : [{'60kg' : '1x8'}, {'70kg' : '1x9'}, {'60kg' : '1x10'}]}
+]
 
 @main_routes.route("/")
 def home():
-    return "Hello, this is a basic flask app."
+    return jsonify({'workouts' : all_workouts})
+
+@main_routes.route('/about')
+def about():
+    return "About page"
